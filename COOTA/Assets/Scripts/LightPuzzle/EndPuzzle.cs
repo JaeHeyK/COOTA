48e54943efd97676f5146fd5cad6d332cf6eae6d
Cheongsol <63346007+Cheongsol@users.noreply.github.com>
2020-09-17 21:28:15 +0900

    Scene_Alley
    Alley 씬 수정함.

---
+++ COOTA/Assets/Scripts/LightPuzzle/EndPuzzle.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndPuzzle : MonoBehaviour
+{
+    private bool isTouching = false;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+            isTouching = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+            isTouching = false;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isTouching == true && Input.GetButtonDown("Fire1"))
+        {
+            SceneManager.LoadScene("Alley", LoadSceneMode.Single);
+
+        }
+
+    }
+}
