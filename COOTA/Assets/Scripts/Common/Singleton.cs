d2d4aa3f1ebd668aa93dd628371100deeb841efb
derder543 <nibya1322@gmail.com>
2020-10-27 22:09:25 +0900

    Singleton, YieldInstructionCache 추가

---
+++ COOTA/Assets/Scripts/Common/Singleton.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+// Generic Singleton 정의
+// Singleton은 하나만 존재해야 한다는 원칙을 준수해야 한다.
+public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+
+    // Singleton 변수는 이 프로퍼티를 통해 참조한다.
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {                
+                T[] s = FindObjectsOfType<T>();
+
+                // 찾은 오브젝트가 1개 이상일 때 에러 메세지를 띄움
+                if (s.Length > 1)
+                {
+                    string log = typeof(T).ToString() + " has one more instance. :";
+
+                    for(int i = 0; i < s.Length; i++)
+                    {
+                        log += " " + s[i].name;
+                    }
+
+                    Debug.LogWarning(log);
+                }
+                // 해당 타입의 오브젝트가 없는 경우 null return
+                else if (s.Length == 0)
+                {
+                    Debug.LogError(typeof(T).ToString() + " is not exist.");
+
+                    return null;
+                }
+
+                instance = s[0];
+            }
+
+            return instance;
+        }
+    }
+}
---
+++ COOTA/Assets/Scripts/Common/YieldInstructionCache.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Coroutine yield return 에서 GC(Garbage Collection) 호출 최소화 하기위해 사용.
+ * 출처 : https://ejonghyuck.github.io/blog/2016-12-12/unity-coroutine-optimization/ */
+
+internal static class YieldInstructionCache
+{
+    class FloatComparer : IEqualityComparer<float>
+    {
+        bool IEqualityComparer<float>.Equals(float x, float y)
+        {
+            return x == y;
+        }
+        int IEqualityComparer<float>.GetHashCode(float obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+
+    public static readonly WaitForEndOfFrame WaitForEndOfFrame = new WaitForEndOfFrame();
+    public static readonly WaitForFixedUpdate WaitForFixedUpdate = new WaitForFixedUpdate();
+
+    private static readonly Dictionary<float, WaitForSeconds> _timeInterval = new Dictionary<float, WaitForSeconds>(new FloatComparer());
+
+    public static WaitForSeconds WaitForSeconds(float seconds)
+    {
+        WaitForSeconds wfs;
+        if (!_timeInterval.TryGetValue(seconds, out wfs))
+            _timeInterval.Add(seconds, wfs = new WaitForSeconds(seconds));
+        return wfs;
+    }
+}
