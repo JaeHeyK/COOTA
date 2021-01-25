dd43a7d87a9d95429467c0fd1a7b205ead0581fe
derder543 <nibya1322@gmail.com>
2021-01-25 23:33:36 +0900

    [맵] 맵 이동기능 구현. Scene으로 각 맵을 관리하도록 변경

---
+++ Assets/Scripts/Scene/TransferScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransferScene : MonoBehaviour
+{
+    [Header("Scene")]
+    [SerializeField]
+    private string transferSceneName = "";   // 로드 할 씬 이름
+
+    [SerializeField]
+    private Transform trMovePosition = null;  // 캐릭터가 이동할 위치
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag.Equals("Player"))
+        {
+            SceneManager.LoadScene(transferSceneName);
+        }
+    }
+}
