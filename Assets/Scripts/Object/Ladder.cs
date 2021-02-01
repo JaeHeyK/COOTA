1f624953f482fcb963d44e9fd58953ad5068cb03
derder543 <nibya1322@gmail.com>
2021-02-01 23:24:55 +0900

    [코딩] 사다리 작업 구상

---
+++ Assets/Scripts/Object/Ladder.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ladder : MonoBehaviour
+{
+    private enum LadderPart { complete, bottom, top };
+    [SerializeField] LadderPart part = LadderPart.complete;
+
+    private PlayerController playerController;
+
+    void Start()
+    {
+        playerController = PlayerController.Instance;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            switch (part)
+            {
+                case LadderPart.complete:
+                    break;
+                case LadderPart.bottom:
+                    break;
+                case LadderPart.top:
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            switch (part)
+            {
+                case LadderPart.complete:
+                    break;
+                case LadderPart.bottom:
+                    break;
+                case LadderPart.top:
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
