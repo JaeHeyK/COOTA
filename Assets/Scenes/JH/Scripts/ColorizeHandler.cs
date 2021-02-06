89bdeae577de8e4670d9545b12ef1d5fde61f865
JaeHeyK <jaeheyk@gmail.com>
2021-02-07 04:47:49 +0900

    [애니메이션] 은신처 중간층 도착 시 실행되는 ColorizeTimeline 추가

---
+++ Assets/Scenes/JH/Scripts/ColorizeHandler.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+//플레이어가 Collider에 감지되었을 때 ColorizePlay 타임라인을 실행시키는 핸들러
+[RequireComponent(typeof(PlayableDirector))]
+public class ColorizeHandler : MonoBehaviour
+{
+    void Start()
+    {
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (enabled && other.CompareTag("Player"))
+        {
+            GetComponent<PlayableDirector>().Play();
+            enabled = false;
+        }
+    }
+}
